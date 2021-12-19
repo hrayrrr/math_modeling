8d629da6975ef6905f5bf23b9084c04848b2bd9b
hrayr <94626565+hrayrrr@users.noreply.github.com>
2021-12-19 19:36:24 +0000

    3

--- lab_3_task3.py
+++ lab_3_task3.py
@@ -1,16 +1,23 @@
 import numpy as np
+
 from physical_constants import g
-x0 = 13
-y0 = 69
-vx0 = 46
-vy0 = 87
+
+x0 = 15
+y0 = 20
+vx0 = 45
+vy0 = 30
 N = 10
+
 coords = np.zeros((N, 3))
+
 t = np.linspace(0, 5, N)
+
 x = x0 + vx0 * t
 y = y0 + vy0 * t - g * t**2 / 2
+
 for i in range(N):
   coords[i, 0] = t[i]
   coords[i, 1] = x[i]
   coords[i, 2] = y[i]
+
 print(coords)
