cc52cbb374b772aac67a1ea0828a9b6a9768737f
hrayr <94626565+hrayrrr@users.noreply.github.com>
2022-01-13 15:24:40 +0000

    lab_4_task5

--- lab_4_task5.py
+++ lab_4_task5.py
@@ -4,29 +4,22 @@ r = 15
 a = 20
 b = 35
 c = 10
-
-def circle(r):
-  S = np.pi * (r ** 2)
-  return S
-
-def triangle(a, b, c):
-  S = a * b * c
-  return S
-
-def square(a, b):
-  S = a * b
-  return S
+h = 12
 
 q = float(input('Выберите фигуру:\n\n1 - Круг\n2 - Треугольник\n3 - Прямоугольник\n\nВыбор - '))
 
-if q == 1:
-  print('\nОтвет:')
-  print(circle(r))
-elif q == 2:
-  print('\nОтвет:')
-  print(triangle(a, b, c))
-elif q == 3:
-  print('\nОтвет:')
-  print(square(a, b))
-else:
-  print('\nОшибка :(')
+def geometry(a, b, c, r, h, q):
+  circle = np.pi * (r ** 2)
+  triangle = 0.5 * a * h
+  square = a * b
+  error = 'Ошибка'
+  if q == 1:
+   return circle
+  elif q == 2:
+   return triangle
+  elif q == 3:
+   return square
+  else:
+    return error
+
+print(geometry(a, b, c, r, h, q))
