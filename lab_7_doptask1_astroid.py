384c2ae422590e09cd4c4a8c5f1c142996822607
hrayrrr <94626565+hrayrrr@users.noreply.github.com>
2022-04-14 16:20:32 +0000

    less7

--- lab_7_doptask1_astroid.py
+++ lab_7_doptask1_astroid.py
@@ -2,24 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-fig = plt.figure(facecolor='black')
+fig = plt.figure(facecolor='white')
 ax = plt.subplot()
-ax.axis('off')
 
 t = np.linspace(0, 30*np.pi, 400)
 
 R = 37
 r = 15
 
-x = ((R - r) * np.cos(t)) + (r * np.cos(t * ((R - r)/r)))
-y = ((R - r) * np.sin(t)) - (r * np.sin(t * ((R - r)/r)))
+x = R * (np.cos(t)**3)
+y = R * (np.sin(t)**3)
 
 phi = np.linspace(0, 10*np.pi, 100)
 
 lcircle_x = R*np.cos(phi)
 lcircle_y = R*np.sin(phi)
 
-ax.plot(lcircle_x, lcircle_y, 'gray', lw=1)
+ax.plot(lcircle_x, lcircle_y, 'black', lw=1)
 
 scircle_x = r*np.cos(phi)
 scircle_y = r*np.sin(phi)
@@ -37,16 +36,16 @@ def animate(i):
     astroids.pop().remove()
     points.pop().remove()
     
-  circle, = ax.plot(dx[i]+scircle_x, dy[i]+scircle_y, 'purple', lw=0.5)
-  astroid, = ax.plot(x[:i+1], y[:i+1], 'white', lw=2)
+  circle, = ax.plot(dx[i]+scircle_x, dy[i]+scircle_y, 'black', lw=0.5)
+  astroid, = ax.plot(x[:i+1], y[:i+1], 'blue', lw=2)
   point, = ax.plot(x[i], y[i], 'ro', markersize=10)
   
   circles.append(circle)
   astroids.append(astroid)
   points.append(point)
   
-  return fig,
-  
-ani = FuncAnimation(fig, animate, frames=400, interval=50, blit=False, repeat_delay=2000)
+ax.axis('equal')
+
+ani = FuncAnimation(fig, animate, frames=240, interval=40, repeat_delay=2000)
 
 ani.save('lab_7_doptask1_astroid.gif')
